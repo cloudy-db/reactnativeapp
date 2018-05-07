68f1f401deb1e0136e0d9d5fd99df892d5ceb359
LCHCurtis <31716275+LCHCurtis@users.noreply.github.com>
2018-05-07 20:49:02 +0800

    package

--- ReactNativeApp/App.js
+++ ReactNativeApp/App.js
@@ -5,6 +5,8 @@
  */
 
 import React, { Component } from 'react';
+import BottomNavigation, { Tab } from 'react-native-material-bottom-navigation';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import {
   Platform,
   StyleSheet,
@@ -23,7 +25,7 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
+      /*<View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
@@ -33,9 +35,78 @@ export default class App extends Component<Props> {
         <Text style={styles.instructions}>
           {instructions}
         </Text>
-      </View>
+      </View>*/
+        <BottomNavigation
+            labelColor="white"
+            rippleColor="white"
+            style={{
+                height: 56,
+                elevation: 8,
+                position: 'absolute',
+                left: 0,
+                bottom: 0,
+                right: 0
+            }}
+            onTabChange={newTabIndex => alert(`New Tab at position ${newTabIndex}`)}
+        >
+            <Tab
+                barBackgroundColor="#37474F"
+                label="dashboard"
+                icon={<Icon size={24} color="white" name="dashboard" />}
+            />
+            <Tab
+                barBackgroundColor="#37474F"
+                label="Music"
+                icon={<Icon size={24} color="white" name="music-note" />}
+            />
+            <Tab
+                barBackgroundColor="#37474F"
+                label="Books"
+                icon={<Icon size={24} color="white" name="book" />}
+            />
+        </BottomNavigation>
     );
   }
+}
+class MyComponent extends Component {
+    render() {
+        return (
+            <BottomNavigation
+                labelColor="white"
+                rippleColor="white"
+                style={{
+                    height: 56,
+                    elevation: 8,
+                    position: 'absolute',
+                    left: 0,
+                    bottom: 0,
+                    right: 0
+                }}
+                onTabChange={newTabIndex => alert(`New Tab at position ${newTabIndex}`)}
+            >
+                <Tab
+                    barBackgroundColor="#37474F"
+                    label="Movies & TV"
+                    icon={<Icon size={24} color="white" name="tv" />}
+                />
+                <Tab
+                    barBackgroundColor="#00796B"
+                    label="Music"
+                    icon={<Icon size={24} color="white" name="music-note" />}
+                />
+                <Tab
+                    barBackgroundColor="#5D4037"
+                    label="Books"
+                    icon={<Icon size={24} color="white" name="book" />}
+                />
+                <Tab
+                    barBackgroundColor="#3E2723"
+                    label="Newsstand"
+                    icon={<Icon size={24} color="white" name="newspaper" />}
+                />
+            </BottomNavigation>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
