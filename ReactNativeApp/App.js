566be91e7d2ef493ce81855dc994c0ac3a1f5287
LCHCurtis <31716275+LCHCurtis@users.noreply.github.com>
2018-05-08 14:31:06 +0800

    3 tab

--- ReactNativeApp/App.js
+++ ReactNativeApp/App.js
@@ -5,6 +5,8 @@
  */
 
 import React, { Component } from 'react';
+import { MaterialIcons } from 'react-native-vector-icons/MaterialIcons'; // Version can be specified in package.json
+import { TabNavigator, TabBarBottom } from 'react-navigation'; // Version can be specified in package.json
 import {
   Platform,
   StyleSheet,
@@ -12,47 +14,67 @@ import {
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+class DashboardScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Home!</Text>
+            </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+class ActivityScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Settings!</Text>
+            </View>
+        );
+    }
+}
+
+class NewBillScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Settings!</Text>
+            </View>
+        );
+    }
+}
+
+export default TabNavigator(
+    {
+        Dashboard: { screen: DashboardScreen },
+        Activity: { screen: ActivityScreen },
+        NewBill: {screen: NewBillScreen}
+    },
+    {
+        navigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, tintColor }) => {
+                const { routeName } = navigation.state;
+                let iconName;
+                if (routeName === 'Dashboard') {
+                    iconName = "dashboard";
+                } else if (routeName === 'Activity') {
+                    iconName = "activity";
+                } else if (routeName === 'NewBill') {
+                    iconName = "bill";
+                }
+                // <MaterialIcons name="icon-name" size={20} color="#4F8EF7" />
+                // You can return any component that you like here! We usually use an
+                // icon component from react-native-vector-icons
+                return <MaterialIcons name={ iconName } size={20} color="#4F8EF7" />;
+            },
+        }),
+        //tabBarComponent: TabBarBottom,
+        //tabBarPosition: 'bottom',
+        tabBarOptions: {
+            activeTintColor: 'orange',
+            inactiveTintColor: 'white',
+        },
+        animationEnabled: true,
+        swipeEnabled: true,
+    }
+);
