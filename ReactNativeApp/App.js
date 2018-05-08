d797a28274309987da764b45dc3d07610e493628
LCHCurtis <31716275+LCHCurtis@users.noreply.github.com>
2018-05-08 22:33:24 +0800

    view

--- ReactNativeApp/App.js
+++ ReactNativeApp/App.js
@@ -4,20 +4,25 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import { MaterialIcons } from 'react-native-vector-icons/MaterialIcons'; // Version can be specified in package.json
-import { TabNavigator, TabBarBottom } from 'react-navigation'; // Version can be specified in package.json
+import React, {Component} from 'react';
+import {MaterialIcons} from 'react-native-vector-icons/MaterialIcons'; // Version can be specified in package.json
+import {TabNavigator, TabBarBottom} from 'react-navigation'; // Version can be specified in package.json
+
+import {Hoshi} from 'react-native-textinput-effects';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    ScrollView,
+    TouchableOpacity,
+    KeyboardAvoidingView
 } from 'react-native';
 
 class DashboardScreen extends React.Component {
     render() {
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
                 <Text>Home!</Text>
             </View>
         );
@@ -27,33 +32,27 @@ class DashboardScreen extends React.Component {
 class ActivityScreen extends React.Component {
     render() {
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Settings!</Text>
-            </View>
-        );
+            <ScrollView>
+            </ScrollView>
+        )
     }
 }
 
+
 class NewBillScreen extends React.Component {
-    render() {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Settings!</Text>
-            </View>
-        );
-    }
+    
 }
 
 export default TabNavigator(
     {
-        Dashboard: { screen: DashboardScreen },
-        Activity: { screen: ActivityScreen },
+        Dashboard: {screen: DashboardScreen},
+        Activity: {screen: ActivityScreen},
         NewBill: {screen: NewBillScreen}
     },
     {
-        navigationOptions: ({ navigation }) => ({
-            tabBarIcon: ({ focused, tintColor }) => {
-                const { routeName } = navigation.state;
+        navigationOptions: ({navigation}) => ({
+            tabBarIcon: ({focused, tintColor}) => {
+                const {routeName} = navigation.state;
                 let iconName;
                 if (routeName === 'Dashboard') {
                     iconName = "dashboard";
@@ -65,7 +64,7 @@ export default TabNavigator(
                 // <MaterialIcons name="icon-name" size={20} color="#4F8EF7" />
                 // You can return any component that you like here! We usually use an
                 // icon component from react-native-vector-icons
-                return <MaterialIcons name={ iconName } size={20} color="#4F8EF7" />;
+                return <MaterialIcons name={iconName} size={20} color="#4F8EF7"/>;
             },
         }),
         //tabBarComponent: TabBarBottom,
@@ -78,3 +77,58 @@ export default TabNavigator(
         swipeEnabled: true,
     }
 );
+
+let styles = {
+    scroll: {
+        backgroundColor: '#E8EAF6',
+    },
+
+    container: {
+        margin: 8,
+        marginTop: 24
+    },
+
+    contentContainer: {
+        padding: 8,
+    },
+};
+
+const textfield = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 24,
+        padding: 20,
+        backgroundColor: 'white',
+    },
+    content: {
+        // not cool but good enough to make all inputs visible when keyboard is active
+        paddingBottom: 300,
+    },
+    card1: {
+        paddingVertical: 16,
+    },
+    card2: {
+        padding: 16,
+    },
+    input: {
+        marginTop: 4,
+        marginBottom: 10,
+    },
+    title: {
+        paddingBottom: 16,
+        textAlign: 'center',
+        color: '#404d5b',
+        fontSize: 20,
+        fontWeight: 'bold',
+        opacity: 0.8,
+    },
+    buttonContainer: {
+        backgroundColor: '#2980b9',
+        paddingVertical: 15
+    },
+    buttonText: {
+        textAlign: 'center',
+        color: '#FFFFFF',
+        fontWeight: '700'
+    }
+});
